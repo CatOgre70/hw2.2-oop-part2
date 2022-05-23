8a3fd1fed4ba376f2748c1071591d8339c2ef8a3
Vasily Demin <vasily.demin@gmail.com>
2022-05-23 22:17:46 +0300

    Homework 2.2, OOP, part 2, all tasks, abstract class Transport added (minor changes)

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -13,20 +13,7 @@ public class Bicycle extends Transport{
                 '}';
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Bicycle bicycle = (Bicycle) o;
-        return super.getWheelsCount() == bicycle.getWheelsCount() && super.getModelName().equals(bicycle.getModelName());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.getModelName(), super.getWheelsCount());
-    }
-
-    public void check() {
+   public void check() {
         System.out.println("Обслуживаем " + this.getModelName());
         for (int i = 0; i < this.getWheelsCount(); i++) {
             this.updateTyre(i + 1);
--- src/Truck.java
+++ src/Truck.java
@@ -4,7 +4,9 @@ public class Truck extends Transport{
         super(modelName, wheelsCount);
     }
 
-    public void checkEngine(){System.out.println("Проверяем двигатель");}
+    public void checkEngine(){
+        System.out.println("Проверяем двигатель");
+    }
 
     public void checkTrailer(){
         System.out.println("Проверяем прицеп");
